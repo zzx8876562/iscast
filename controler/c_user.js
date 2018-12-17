b387ccc2123d71d52ca149ad760fce2555dd711c
郑志旭 <516532458@qq.com>
2018-12-17 09:46:57 +0800

    完成了服务端登录功能

--- controler/c_user.js
+++ controler/c_user.js
@@ -4,7 +4,7 @@
 // 2. 配置
 const connection = mysql.createConnection({
     // 主机
-    host: 'localhost',
+    host: '127.0.0.1',
     // 用户名
     user: 'root',
     // 密码
@@ -21,12 +21,60 @@ connection.connect();
       res.render('signin.html');
   }
   //res
-  let sqlstr="select *from `user` where email=?";
-  //执行数据库语句
-  connection.query(sqlstr,(err,data)=>{
+  exports.formSubmit=(req,res)=>{
 
-  })
-  //处理数据
+    // res.send('//////');
+    let body =req.body;
+    console.log(body);
+    let email=body.email;
+    let password=body.password;
+
+    //设置sql语句
+    let sqlstr="select * from `users` where email=?";
+    //执行sql语句
+     connection.query(sqlstr,email,(err,result)=>{
+       //处理结果
+       if(err){
+         throw err;
+       }
+       console.log(result);
+
+      //  返回为一个数组  如果数组不为空 说明查询到了  验证emai
+      //  数组为空说明 该账号不存在
+       if(result.length==0){
+        //  console.log(result);
+           res.send({
+             "code":'0',
+             "meg":'该用户不存在'
+           })
+       }
+       if(result[0].password!=password){
+         return  res.send({
+           "code":400,
+           "msg":"密码错误"
+
+         })
+       }
+       if(result[0].password==password){
+         res.send({
+           "code":200,
+           "msg":'登录成功'
+         })
+       }
+      // if(result.length!=0){
+      //     res.send('账号正确');
+      // }
+
+
+     })
+    // let sqlstr="select *from `user` where email=?";
+    // //执行数据库语句
+    // connection.query(sqlstr,(err,data)=>{
+  
+    // })
+    // 处理数据
+  }
+
  
   // 5. 关闭数据库
-connection.end();
+// connection.end();
